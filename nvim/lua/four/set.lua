32885aeb9dd05e8523d97e92a6544f44394981b8
Four <f0uronvacation@protonmail.com>
2023-05-13 14:18:58 +0200

    Update set.lua

--- nvim/lua/four/set.lua
+++ nvim/lua/four/set.lua
@@ -20,25 +20,3 @@ vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
 vim.g.mapleader = " "
-
--- local opts = {
---     backup = false,
---     clipboard = "unnamedplus",
---     guicursor = "",
---     number = true,
---     relativenumber = true,
---     fileencoding = "utf-8",
--- 	tabstop = 4,
--- 	shiftwidth = 4,
--- 	expandtab = true,
--- 	softtabstop = 4,
---     completeopt = { "menu", "menuone", "noselect" } -- cmp options
--- }
-
--- for k, v in pairs(opts) do
---     vim.opt[k] = v
--- end
-
--- vim.cmd [[
---     set mouse=
--- ]]
